4f895f081b00e849c8ca3e5ec52fd57c4106edfb
gopheramit <amitwpu@gmail.com>
2022-09-04 21:29:11 +0530

    first api call

--- api_basics/views.py
+++ api_basics/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+from django.http.response import HttpResponse,JsonResponse 
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serilizers import ArticleSerializer
 
 # Create your views here.
+
+def article_list(request):
+    if request.method=="GET":
+        articles=Article.objects.all()
+        serilizer=ArticleSerializer(articles,many=True)
+        return JsonResponse(serilizer.data, safe=False)
+
+    elif request.method=="POST":
+        data=JSONParser().parse(request)
+        serilizer=ArticleSerializer(data=data)
+
+        if serilizer.is_valid():
+            serilizer.save()
+            return JsonResponse(serilizer.data ,status=201)
+        return JsonResponse(serilizer.errors,status=400)
+
+
